2e2a41a2eb759b1d06f1eafbb671defe970e409b
Ruben Shekspir <shekspir55@gmail.com>
2026-03-18 15:11:30 +0400

    Fix lint errors: replace empty interfaces with type aliases, use ESM import for tailwindcss-animate

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,13 @@
 import { createRoot } from "react-dom/client";
+import posthog from "posthog-js";
 import App from "./App.tsx";
 import "./index.css";
+
+posthog.init("phc_ay2mNbUTujRuE0756cu5X3wpvX8BI9QnOb32ZFqlOu0", {
+  api_host: "https://eu.i.posthog.com",
+  session_recording: {
+    maskAllInputs: false,
+  },
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
